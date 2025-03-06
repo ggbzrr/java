504365c22d6e838001ab05707f8ca7dba7d5713a
GabrielBezerra <ggbzrr@yahoo.com.br>
2025-03-05 23:25:22 -0300

    teste commit

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("agora com github!");
+        System.out.println("agora com github! alterado para teste");
     }
 }
